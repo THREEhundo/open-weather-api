e7f0202bad4e6542dcdad33ec9a90b981f07398a
threehundo <samuelbaik87@gmail.com>
2021-10-25 10:56:33 -0700

    Template for API data retrieved

---
+++ src/App/weatherTemplate.js
@@ -0,0 +1,19 @@
+const weatherTemplate = {
+		city: "Los Angeles",
+		country: "US",
+		feels_like: 68.99,
+		humidity: 67,
+		temperature: 69.22,
+		max_temp: 62.55,
+		min_temp: 60.44,
+		description: "clear sky",
+		wind: 0,
+		sunrise: 1634911438,
+		sunset: 1634951429,
+		icon_id: 800,
+		icon_name: "01d",
+		locale_time: 1634938504,
+		timezone: -25200,
+}
+
+export default weatherTemplate;
